c7a0b2a97549dfb164dcd64ee4e6a287bad1d9d1
Hitesh Patil <hiteshpatil0205@gmail.com>
2025-07-31 10:24:28 +0530

    Atbash

--- Assignment-1/Substitution Cipher/Atbash.py
+++ Assignment-1/Substitution Cipher/Atbash.py
@@ -1,4 +1,4 @@
-def AtbashCipher(text):
+def AtbashCipherEncrypt(text):
     result = ""
     for ch in text:
         if ch.isupper():
@@ -9,6 +9,21 @@ def AtbashCipher(text):
             result += ch 
     return result
 
+def AtbashCipherDecrypt(text):
+    decryptResult = ""
+    for ch in text:
+        if ch.isupper():
+            decryptResult += chr(ord('A') + (ord('Z') - ord(ch)))
+        elif ch.islower():
+            decryptResult += chr(ord('a') + (ord('z') - ord(ch)))
+        else:
+            decryptResult += ch 
+    return decryptResult
+
 text = input("Enter the text for Atbash Cipher: ")
-ciphered = AtbashCipher(text)
+
+ciphered = AtbashCipherEncrypt(text)
 print("Atbash Cipher Output:", ciphered)
+
+decrypted = AtbashCipherDecrypt(ciphered)
+print("Decrypted Text:", decrypted)
